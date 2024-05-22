083f01de801748b51cc9ba5393990461f89b88fc
Vilém Charwot <mrlogen1299@gmail.com>
2024-05-22 20:54:14 +0200

    added gitignore and dependency on boostrap and oauth2-facebook

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+const COMMON_PATH = '/www/BookBookGo';
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo COMMON_PATH;?>/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <title>BookBookGo</title>
+</head>
+<body>
+    <div class="container">
+        
+    </div>
+
+    <script src="<?php echo COMMON_PATH;?>/vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
